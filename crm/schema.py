e44e044e050b6d6853d101aa069768ad673cf311
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-08-31 15:17:51 +0300

    Add UpdateLowStockProducts mutation and cron job

--- crm/schema.py
+++ crm/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 from .models import Customer, Product, Order
+from crm.models import Product
 
 
 # ---------------- Types ----------------
